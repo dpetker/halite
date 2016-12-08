a1f6c642daaa42e8f39b87ab71f6e5ab0439dced
Dan Petker <dan.petker@vidyard.com>
2016-12-07 21:33:47 -0500

    Always attack weakest neighbour

--- MyBot.py
+++ MyBot.py
@@ -8,10 +8,20 @@ def create_move(location):
   site = gameMap.getSite(location)
 
   # See if there's an enemy adjacent to us with less strength. If so, capture it
+  max_delta = 0
+  attack_direction = None
   for d in CARDINALS:
     neighbour_site = gameMap.getSite(location, d)
     if neighbour_site.owner != myID and neighbour_site.strength < site.strength:
-      return Move(location, d)
+
+      # Go for the weakest neighbour
+      temp_delta = site.strength - neighbour_site.strength
+      if temp_delta > max_delta:
+        max_delta = temp_delta
+        attack_direction = d
+
+  if attack_direction is not None:
+    return Move(location, attack_direction)
 
   # Don't move until we're sufficiently strong
   if site.strength < site.production * 5:
